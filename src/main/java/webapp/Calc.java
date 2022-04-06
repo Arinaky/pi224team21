680950e3daf5d5f9e0d65d90d0ad180d722dfdb2
Ilzida <il.khizbullina@yandex.ru>
2022-04-06 16:53:08 +0500

    Fix Calc.java

--- src/main/java/webapp/Calc.java
+++ src/main/java/webapp/Calc.java
@@ -1,19 +1,27 @@
 package webapp;
 
-@WebServlet(name="Calc" urlPatterns="/JavaCalc")
-public class Calc extend HttpServlet {
+import java.io.IOException;
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) {
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name="Calc", urlPatterns="/JavaCalc")
+public class Calc extends HttpServlet {
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		RequestCalc Calc = RequestCalc.fromRequestParameters(request);
 		Calc.setAsRequestAttributesAndCalculate(request);
 		
-		request.getRequestDispatcher("/Result.jsp").forward(request, response)
+		request.getRequestDispatcher("/Result.jsp").forward(request, response);
 	}
 	
 	private static class RequestCalc {
 		private final String radius_calc;
 		private final String height_calc;
-		private int result;
+		private double result;
 		
 		public RequestCalc(String radius, String height) {
 			this.radius_calc = radius;
@@ -22,13 +30,14 @@ public class Calc extend HttpServlet {
 		
 		public static RequestCalc fromRequestParameters(HttpServletRequest request) {
 			return new RequestCalc(
-			request.getParameter("radius");
+			request.getParameter("radius"),
 			request.getParameter("height"));
 		}
 		
 		public void setAsRequestAttributesAndCalculate(HttpServletRequest request) {
-			request.setAttribute(radius_calc);
-			request.setAttribute(height_calc);
+			
+			request.setAttribute("radius", radius_calc);
+			request.setAttribute("height", height_calc);
 			int radius_try;
 			int height_try;
 			try {
@@ -38,7 +47,7 @@ public class Calc extend HttpServlet {
 				radius_try=0;
 				height_try=0;
 			}
-			result=1/3*Math.PI*Math,pow(radius_try, 2)*height_try
+			result=1/3*Math.PI*Math.pow(radius_try, 2)*height_try;
 		}
 	}
 }
